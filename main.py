af1004673048da840a11ad7b42f15d26ad8f9705
aksalcido <aksalcid@uci.edu>
2021-05-04 01:56:23 -0700

    flask transition start

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-import audio_extractor as audio
+import audio_extractor as audio_extract
 
 def main():
-    videoInput, audioFile = audio.filesInput()
+    videoInput, audioFile = audio_extract.filesInput()
 
     # Automatically Ends the Program if the User doesn't want to Overwrite
-    if audio.avoidOverWriting(videoInput, audioFile):
+    if audio_extract.avoidOverWriting(videoInput, audioFile):
         return
 
     else:
         # YouTube links are handled different as requires a download
-        if audio.youtubeLink(videoInput):
-            audio.youtubeDownload(videoInput, audioFile)
+        if audio_extract.youtubeLink(videoInput):
+            audio_extract.youtubeDownload(videoInput, audioFile)
 
         # Handles the videos that are already in the directory
         else:
-            audio.convertVideo(videoInput, audioFile)
+            audio_extract.convertVideo(videoInput, audioFile)
 
 
 if __name__ == '__main__':
